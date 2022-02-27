610c5d7036051a3d1e60674df5b7f48e746ac9aa
Jami Orris <81092462+jamimo76@users.noreply.github.com>
2022-02-27 17:02:22 -0500

    need Navbar, Home, About, Contact us updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Directory from "./components/DirectoryComponents";
+import Navbar from "./components/Navbar";
+
 import "./index.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar light>
-          <div className="container">
-            <NavbarBrand href="/">Rockingham Commons</NavbarBrand>
-          </div>
-        </Navbar>
-        <Directory />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,51 @@
 import React from "react";
-import {
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  NavItem,
-  Jumbotron,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Form,
-  FormGroup,
-  Input,
-  Label
-} from "reactstrap";
-import { NavLink } from "react-router-dom";
+// import {
+//   Nav,
+//   Navbar,
+//   NavbarBrand,
+//   NavbarToggler,
+//   Collapse,
+//   NavItem,
+//   Jumbotron,
+//   Button,
+//   Modal,
+//   ModalHeader,
+//   ModalBody,
+//   Form,
+//   FormGroup,
+//   Input,
+//   Label
+// } from "reactstrap";
+// import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <Navbar>
-          
-        </Navbar>
+    <div className="navbar">
+      <div className="logo">
+        <img
+          src="..\assets\images\RCCA sign.PNG"
+          height="50"
+          width="50"
+          alt="Rockingham Commons"
+        />
+      </div>
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <a href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="/about">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a href="/contact">Contact Us</a>
+        </li>
+      </ul>
+      <div className="hamburger">
+        <FaBars size={30} style={{ color: "#f8f8f8" }} />
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
