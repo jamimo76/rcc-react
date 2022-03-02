fc856655222243ea29a4bc1379b3bffd905248d5
Jami Orris <81092462+jamimo76@users.noreply.github.com>
2022-03-02 05:56:21 -0500

    not using FA Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Nav, NavItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
-// import { FaBars, FaTimes } from "react-icons/fa";
 
 class Navbar extends Component {
   render() {
